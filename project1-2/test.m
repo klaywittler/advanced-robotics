e39bdfd66590ac8b5fdeff6335ca8b4b3d0e2b8b
klaywittler <klaywittler@gmail.com>
2019-02-11 13:42:13 -0500

    .

--- project1-2/test.m
+++ project1-2/test.m
@@ -6,11 +6,11 @@ addpath('utils')
 addpath('submission')
 
 astar = false;
-% map = load_map('map0.txt',0.5,1,0.1);
-map = load_map('map1.txt',0.1,1,0.1);
+map = load_map('map0.txt',0.1,0.5,0.1);
+% map = load_map('map1.txt',0.1,1,0.1);
 profile on
-% [path, num_expanded] = dijkstra(map,[0,3,5],[20,3,5],astar);
-[path, num_expanded] = dijkstra(map,[0,-5,3],[5,20,5],astar);
+[path, num_expanded] = dijkstra(map,[0,1,5],[20,3,0],astar);
+%[path, num_expanded] = dijkstra(map,[0,-5,3],[5,20,5],astar);
 profile report
 profile off
 plot_path(map,path);
