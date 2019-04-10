95338a318a525539c7c544b9ab4f650dc3e847c3
klaywittler <klaywittler@gmail.com>
2019-04-10 16:16:25 -0400

    accounted for translation vel difference between camera and imu

--- project2-2/submission/init_script.m
+++ project2-2/submission/init_script.m
@@ -11,4 +11,4 @@ Kinv = [311.0520, 0, 201.8724; 0, 311.3885, 113.6210; 0, 0, 1]\eye(3);
 T = [-0.04; 0; -0.03];
 R = eulzxy2rot([pi,0,-44.5*pi/180]);
 
-estimate_vel_handle = @(sensor) estimate_vel(sensor, Kinv, pA);
+estimate_vel_handle = @(sensor) estimate_vel(sensor, Kinv, pA, R, T);
